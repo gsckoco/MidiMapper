75bb328c8e2d2cb2b677d8ef628fb991b9dc4172
gsckmp4 <46426171+gsckmp4@users.noreply.github.com>
2019-03-30 20:17:17 +0000

    Added MIDI input and output device selection. Along with last MIDI message label on Settings tab.

--- src/co/uk/gsck/midi/mapper/Controllers/Main.java
+++ src/co/uk/gsck/midi/mapper/Controllers/Main.java
@@ -1,11 +1,16 @@
 package co.uk.gsck.midi.mapper.Controllers;
 
-import co.uk.gsck.midi.mapper.Handlers.Error;
+import co.uk.gsck.midi.mapper.Handlers.MidiCallback;
 import co.uk.gsck.midi.mapper.Handlers.MidiDeviceInfo;
+import co.uk.gsck.midi.mapper.Handlers.Popup;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 
 import javax.sound.midi.MidiDevice;
@@ -13,40 +18,76 @@ import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Transmitter;
 
+//import co.uk.gsck.midi.mapper.Instructions.;
+
 
 public class Main {
 
-
+    //private ArrayList<>
     @FXML
     private TextArea logTextArea;
     @FXML
-    private ComboBox deviceSelector;
+    private ComboBox indeviceSelector;
+    @FXML
+    private ComboBox outdeviceSelector;
+    @FXML
+    private MenuItem aboutButton;
+    @FXML
+    private Label lastMessage;
 
     private Transmitter transmitter;
 
-    private void log(String message) {
+    public void log(String message) {
         logTextArea.setText( logTextArea.getText() + message + "\n" );
+        System.out.println(message);
+    }
+
+    public void setLastMIDIMessage(String message) {
+        lastMessage.setText(message);
     }
 
     public void initialize() {
         logTextArea.setText("");
         MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
         log("Current devices plugged in are: ");
-        ObservableList<MidiDeviceInfo> midiDevices = FXCollections.observableArrayList();
+        ObservableList<MidiDeviceInfo> inDevices = FXCollections.observableArrayList();
+        ObservableList<MidiDeviceInfo> outDevices = FXCollections.observableArrayList();
         for (int i = 0; i<infos.length; i++) {
             log("["+i+"] " + infos[i].getName() + " : " + infos[i].getDescription());
-            midiDevices.add(new MidiDeviceInfo(infos[i],i));
+            //inDevices.add(new MidiDeviceInfo(infos[i],i));
+            try {
+                if (MidiSystem.getMidiDevice(infos[i]).getMaxTransmitters() != 0) {
+                    inDevices.add(new MidiDeviceInfo(infos[i],i));
+                }
+                if (MidiSystem.getMidiDevice(infos[i]).getMaxReceivers() != 0) {
+                    outDevices.add(new MidiDeviceInfo(infos[i],i));
+                }
+            } catch (MidiUnavailableException e) {
+
+            }
         }
 
-        deviceSelector.setItems(midiDevices);
-        deviceSelector.valueProperty().addListener((obs,oldVal,newVal) -> {
+        aboutButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Popup.info("About","About the Program","MidiMapper  Copyright (C) 2019  Ben Johnston (gsckoco)\n" +
+                        "This program comes with ABSOLUTELY NO WARRANTY.\n" +
+                        "This is free software, and you are welcome to redistribute it\n" +
+                        "under certain conditions. If you would like to learn more goto:\n" +
+                        "https://github.com/gsckoco/MidiMapper/LICENSE.md");
+            }
+        });
+
+        indeviceSelector.setItems(inDevices);
+        outdeviceSelector.setItems(outDevices);
+        indeviceSelector.valueProperty().addListener((obs,oldVal,newVal) -> {
             if (oldVal != null) {
                 try {
                     //MidiDevice oldDev = ((MidiDeviceInfo)oldVal).getDevice();
                     MidiDevice oldDev = (MidiSystem.getMidiDevice(infos[5]));
                     oldDev.close();
                 } catch (MidiUnavailableException e) {
-                    Error.error("Midi device is currently unavailable",e.toString(),true);
+                    Popup.error("Midi device is currently unavailable",e.toString(),true);
                     e.printStackTrace();
                 }
             }
@@ -54,9 +95,9 @@ public class Main {
                 MidiDevice newDev = ((MidiDeviceInfo)newVal).getDevice();
                 newDev.open(); // Open it too allow the listener to
                 transmitter = newDev.getTransmitter();
-                //transmitter.setReceiver(new MidiCallback());
+                transmitter.setReceiver(new MidiCallback(this));
             } catch (MidiUnavailableException e) {
-                Error.error("Midi device is currently unavailable",e.toString(),true);
+                Popup.error("Midi device is currently unavailable",e.toString(),true);
                 e.printStackTrace();
             }
 
--- src/co/uk/gsck/midi/mapper/Handlers/MidiCallback.java
+++ src/co/uk/gsck/midi/mapper/Handlers/MidiCallback.java
@@ -1,12 +1,15 @@
 package co.uk.gsck.midi.mapper.Handlers;
 
+import co.uk.gsck.midi.mapper.Controllers.Main;
+import javafx.application.Platform;
+
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
 
 public class MidiCallback implements Receiver {
-
-    public MidiCallback() {
-
+    private Main main;
+    public MidiCallback(Main main) {
+        this.main = main;
     }
 
     @Override
@@ -14,6 +17,12 @@ public class MidiCallback implements Receiver {
         byte type = message.getMessage()[0];
         byte address = message.getMessage()[1];
         byte value = message.getMessage()[2];
+        //main.log("Midi Message: " + type + ", " + address + ", " + value);
+        Platform.runLater(
+            () -> {
+                main.setLastMIDIMessage((int)type+ ", " + (int)address + ", " + (int)value);
+            }
+        );
 
     }
 
